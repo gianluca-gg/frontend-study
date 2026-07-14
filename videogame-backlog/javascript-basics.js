cfd023ccc1a377d6de96a51d795f81bdca1c5dc2
gianluca-gg <leroyoy7@gmail.com>
2026-07-14 16:38:25 +0200

    folders

---
+++ videogame-backlog/javascript-basics.js
@@ -0,0 +1,14 @@
+const games = [
+  {
+    title: "Tunic",
+    platform: "Switch",
+    status: "da iniziare",
+    rating: null,
+  },
+  {
+    title: "Roadwarden",
+    platform: "PC",
+    status: "completato",
+    rating: 8,
+  },
+];
